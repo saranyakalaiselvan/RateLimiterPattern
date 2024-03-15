0b53036b486c1508d7afbea9f54417a7b4650750
Saranya Kalaiselvan <saranyaselvan@gmail.com>
2024-03-15 20:22:15 +1100

    Rate limiter Changes

--- src/main/java/com/crackit/AddressCircuitBreakerDemo/AddressServiceRateLimiterDemo.java
+++ src/main/java/com/crackit/AddressCircuitBreakerDemo/AddressServiceRateLimiterDemo.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AddressCircuitBreakerDemoApplication {
+public class AddressServiceRateLimiterDemo {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressCircuitBreakerDemoApplication.class, args);
+		SpringApplication.run(AddressServiceRateLimiterDemo.class, args);
 	}
 
 }
